e117a09b8bd377d6383ccf3fe8be59c3ade00305
skim6281 <skim6281@gmail.com>
2017-07-27 18:27:07 -0400

    Add session actions

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,24 @@
 class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      login(@user)
+      render "api/users/show_current_user"
+    else
+      render(json: ["Invalid username/password"], status: 401)
+    end
+  end
+
+  def destroy
+    if logged_in?
+      logout
+      render json: {}
+    else
+      render(json: ['Nobody signed in'], status: 404)
+    end
+  end
 end
