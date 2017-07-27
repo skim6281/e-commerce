7c08e0bd81435b2603dd3531fb32f4c0b5856dd2
skim6281 <skim6281@gmail.com>
2017-07-27 19:12:43 -0400

    Add email

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,6 +3,7 @@ class Api::UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       login(@user)
+      UserMailer.welcome_email(@user).deliver_later
       render 'api/users/user'
     else
       render json: @user.errors.full_messages, status: 422
