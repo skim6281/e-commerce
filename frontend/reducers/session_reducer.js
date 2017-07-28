0513cdfe94f114159816b312ed2da09166725a82
skim6281 <skim6281@gmail.com>
2017-07-28 19:10:10 -0400

    Create Session Reducer

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,30 @@
+import {
+  RECEIVE_CURRENT_USER,
+  RECEIVE_ERRORS,
+  REMOVE_ERRORS,
+  RECEIVE_FORM_TYPE } from '../actions/session_actions';
+import merge from 'lodash/merge';
+
+const initialState = {
+  currentUser: null,
+  errors: [],
+  formType: null
+}
+
+const SessionReducer = (state = initialState, action) => {
+  Object.freeze(state)
+  switch(action.type) {
+    case RECEIVE_CURRENT_USER:
+      return Object.assign({}, state, {currentUser: action.currentUser, errors: []});
+    case RECEIVE_ERRORS:
+      return Object.assign({}, state, { errors: action.errors});
+    case REMOVE_ERRORS:
+      return Object.assign({}, state, { errors: [] });
+    case RECEIVE_FORM_TYPE:
+      return Object.assign({}, state, { formType: action.formType})
+    default:
+      return state;
+  }
+};
+
+export default SessionReducer;
