81e8642c508cb37dc2e4c9c799a27c44e21a623b
skim6281 <skim6281@gmail.com>
2017-07-28 19:22:53 -0400

    Add more imports

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Switch } from 'react-router';
+import { HashRouter, Route } from 'react-router-dom';
+import App from './App';
 
 const Root = ({ store }) => {
   return (
     <Provider store={store}>
-      
+      <HashRouter>
+        <App>
+
+        </App>
+      </HashRouter>
     </Provider>
   )
 }
