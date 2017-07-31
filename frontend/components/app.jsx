4b62fb3e0e17f4fb04971154b3265743dc07e99f
skim6281 <skim6281@gmail.com>
2017-07-31 00:10:26 -0400

    Render auth

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import Nav from './nav';
+import AuthForm from './auth_form';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.renderAuth = this.renderAuth.bind(this);
   }
 
   render() {
-    return (
-      <main>
-        <Nav/>
-        <content className="content">
-          {this.props.children}
-        </content>
-      </main>
-    )
+    const location = window.location.hash;
+    if(location === "#/login") {
+      return(
+        <AuthForm formType="login"/>
+      )
+    } else if(location === "#/signup") {
+      return(
+        <AuthForm formType="signup"/>
+      )
+    } else {
+      return (
+        <main>
+          <Nav/>
+          <content className="content">
+            {this.props.children}
+          </content>
+        </main>
+      )
+    }
   }
 }
 
