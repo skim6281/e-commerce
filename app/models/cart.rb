480f9cfb09b04c7bd417563a4d7b5668c3b37ef8
skim6281 <skim6281@gmail.com>
2017-07-30 21:53:55 -0400

    Annotate cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: carts
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Cart < ActiveRecord::Base
 end
