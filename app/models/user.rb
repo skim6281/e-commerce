d1d42a454f2d30160018ab43241f31ef33045a52
skim6281 <skim6281@gmail.com>
2017-07-30 22:08:48 -0400

    Add user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,9 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6, allow_nil: true }
   after_initialize :ensure_session_token
 
+  has_one :cart
+  has_many :orders
+
   attr_reader :password
 
   def self.find_by_credentials(username, password)
