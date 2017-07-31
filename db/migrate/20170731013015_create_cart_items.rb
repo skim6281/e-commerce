61ae7ed98560c484a83a9d606c62c5c6e1b62e76
skim6281 <skim6281@gmail.com>
2017-07-30 21:39:51 -0400

    Add columns to CartItems table

--- db/migrate/20170731013015_create_cart_items.rb
+++ db/migrate/20170731013015_create_cart_items.rb
@@ -1,8 +1,12 @@
 class CreateCartItems < ActiveRecord::Migration
   def change
     create_table :cart_items do |t|
-
+      t.integer :product_id, null: false
+      t.integer :quantity, null: false
+      t.integer :cart_id, null: false
       t.timestamps null: false
     end
+    add_index :cart_items, :cart_id
+    add_index :cart_items, :product_id
   end
 end
