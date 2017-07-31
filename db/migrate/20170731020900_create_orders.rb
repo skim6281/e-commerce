6d5a2f63802941f3d5a1d06b7ffa2c5ba204605c
skim6281 <skim6281@gmail.com>
2017-07-30 22:12:00 -0400

    Add column to order

--- db/migrate/20170731020900_create_orders.rb
+++ db/migrate/20170731020900_create_orders.rb
@@ -1,8 +1,9 @@
 class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
-
+      t.integer :user_id, null: false
       t.timestamps null: false
     end
+    add_index :orders, :user_id
   end
 end
