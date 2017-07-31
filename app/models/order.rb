ad0416aa613ff2997f5867f39c773751e94b4db6
skim6281 <skim6281@gmail.com>
2017-07-30 22:13:53 -0400

    Annotate order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: orders
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Order < ActiveRecord::Base
 end
