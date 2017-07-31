dfa00ee102481865d8a9216f5491131e704ee644
skim6281 <skim6281@gmail.com>
2017-07-30 22:18:32 -0400

    Annotate orderitem

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: order_items
+#
+#  id         :integer          not null, primary key
+#  product_id :integer          not null
+#  quantity   :integer          not null
+#  order_id   :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class OrderItem < ActiveRecord::Base
 end
