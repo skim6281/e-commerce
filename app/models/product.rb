0a91976cd75a916a712fa73ae558b345afa637b5
skim6281 <skim6281@gmail.com>
2017-07-30 20:53:16 -0400

    Annotate Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  brand      :string           not null
+#  model      :string           not null
+#  sku        :string           not null
+#  price      :integer          not null
+#  desc       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Product < ActiveRecord::Base
 end
