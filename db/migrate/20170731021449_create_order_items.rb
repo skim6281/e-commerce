5b4539ba8406dd405d433107d059e25194a0871b
skim6281 <skim6281@gmail.com>
2017-07-30 22:16:53 -0400

    Add columsn to orderitems

--- db/migrate/20170731021449_create_order_items.rb
+++ db/migrate/20170731021449_create_order_items.rb
@@ -1,8 +1,12 @@
 class CreateOrderItems < ActiveRecord::Migration
   def change
     create_table :order_items do |t|
-
+      t.integer :product_id, null: false
+      t.integer :quantity, null: false
+      t.integer :order_id, null: false
       t.timestamps null: false
     end
+    add_index :order_items, :order_id
+    add_index :order_items, :product_id
   end
 end
