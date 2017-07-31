abf891bfea8e0e965400e5d93d1f41c7d051c502
skim6281 <skim6281@gmail.com>
2017-07-30 22:51:21 -0400

    Add product params

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -7,4 +7,10 @@ class Api::ProductsController < ApplicationController
   def show
 
   end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :brand, :model, :sku, :price, :desc)
+  end
 end
