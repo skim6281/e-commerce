90a278aa1a0fda3f002a5d0829c24f3069d4d8dc
skim6281 <skim6281@gmail.com>
2017-07-31 13:12:50 -0400

    Remove is_admin column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170731021449) do
+ActiveRecord::Schema.define(version: 20170731171126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,12 +66,11 @@ ActiveRecord::Schema.define(version: 20170731021449) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",                        null: false
-    t.string   "password_digest",                 null: false
-    t.string   "session_token",                   null: false
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
-    t.boolean  "is_admin",        default: false, null: false
+    t.string   "username",        null: false
+    t.string   "password_digest", null: false
+    t.string   "session_token",   null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
