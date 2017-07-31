4e307cbf706becf68dd8badeb1d6a02ce2d3d7bb
skim6281 <skim6281@gmail.com>
2017-07-30 20:51:36 -0400

    Add columns to Product

--- db/migrate/20170731004729_create_products.rb
+++ db/migrate/20170731004729_create_products.rb
@@ -1,7 +1,12 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-
+      t.string :name, null: false
+      t.string :brand, null: false
+      t.string :model, null: false
+      t.string :sku, null: false
+      t.integer :price, null: false
+      t.string :desc, null: false
       t.timestamps null: false
     end
   end
