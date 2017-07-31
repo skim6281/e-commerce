67044051d00ad701ec5f47ec835b33690a6eb3e9
skim6281 <skim6281@gmail.com>
2017-07-30 21:39:40 -0400

    Add columns to Cart table

--- db/migrate/20170731012940_create_carts.rb
+++ db/migrate/20170731012940_create_carts.rb
@@ -1,8 +1,9 @@
 class CreateCarts < ActiveRecord::Migration
   def change
     create_table :carts do |t|
-
+      t.integer :user_id, null: false
       t.timestamps null: false
     end
+    add_index :carts, :user_id
   end
 end
