b9487f1eee1b5f14f07012273e400dbfa0258e7c
skim6281 <skim6281@gmail.com>
2017-08-01 17:09:36 -0400

    Add association to product

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -4,7 +4,7 @@
 #
 #  id         :integer          not null, primary key
 #  product_id :integer          not null
-#  quantity   :integer          not null
+#  quantity   :integer
 #  cart_id    :integer          not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
@@ -14,4 +14,5 @@ class CartItem < ActiveRecord::Base
   validates :product_id, :quantity, :cart_id, presence: true
 
   belongs_to :cart
+  belongs_to :product
 end
