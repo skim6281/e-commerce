80eb5a7c62ca160a15d56462ed4fb19d4a06f169
skim6281 <skim6281@gmail.com>
2017-07-31 21:37:17 -0400

    Create Products component

---
+++ frontend/components/products.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Products extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return(<div>Products</div>)
+  }
+}
+
+export default Products;
