396381bbdb3e1d3b20b088d61172041446bb0208
skim6281 <skim6281@gmail.com>
2017-07-31 23:16:31 -0400

    Add ProductReducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import SessionReducer from './session_reducer';
+import ProductReducer from './product_reducer';
 
 const RootReducer = combineReducers({
-  session: SessionReducer
+  session: SessionReducer,
+  product: ProductReducer
 });
 
 export default RootReducer;
