b3190d69748494449e3133853b628685d2a3c310
skim6281 <skim6281@gmail.com>
2017-08-01 17:11:26 -0400

    Add cart items routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   root "static_pages#root"
 
   namespace :api, defaults: {format: :json} do
-    resources :users
+    resources :users do
+      resources :carts do
+        resources :cart_items
+      end
+    end
     resource :session, only: [:create, :destroy, :show]
     resources :products, only: [:index, :create, :show]
   end
