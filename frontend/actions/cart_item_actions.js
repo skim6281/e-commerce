84f1e29b223305ea22f1f7b102c5648f91aa3714
skim6281 <skim6281@gmail.com>
2017-08-01 23:06:22 -0400

    Add removeCartItem action

--- frontend/actions/cart_item_actions.js
+++ frontend/actions/cart_item_actions.js
@@ -2,6 +2,7 @@ import * as CartItemAPIUtil from '../util/cart_item_api_util';
 
 export const RECEIVE_CART_ITEM = 'RECEIVE_CART_ITEM';
 export const RECEIVE_CART_ITEMS = 'RECEIVE_CART_ITEMS';
+export const REMOVE_CART_ITEM = 'REMOVE_CART_ITEM';
 
 export const receiveCartItem = cartItem => ({
   type: RECEIVE_CART_ITEM,
@@ -13,6 +14,11 @@ export const receiveCartItems = cartItems => ({
   cartItems
 });
 
+export const removeCartItem = cartItem => ({
+  type: REMOVE_CART_ITEM,
+  cartItem
+});
+
 export const createCartItem = cartItem => dispatch => {
   return CartItemAPIUtil.createCartItem(cartItem)
     .then(cartItem => dispatch(receiveCartItem(cartItem)));
@@ -21,4 +27,9 @@ export const createCartItem = cartItem => dispatch => {
 export const fetchCartItems = cartId => dispatch => {
   return CartItemAPIUtil.fetchCartItems(cartId)
     .then(cartItems => dispatch(receiveCartItems(cartItems)));
+};
+
+export const deleteCartItem = cartItem => dispatch => {
+  return CartItemAPIUtil.deleteCartItem(cartItem)
+    .then(cartItem => dispatch(removeCartItem(cartItem)));
 };
