cf7453b6252f541595172913e42c24a6e1ecff51
skim6281 <skim6281@gmail.com>
2017-08-02 16:11:52 -0400

    Edit products param

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,6 @@
 class Api::ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.all.order(created_at: :desc)
     render 'api/products/index'
   end
 
