c47c292af4ce94e544680eec5f8b745bf7b7659d
skim6281 <skim6281@gmail.com>
2017-08-02 02:20:11 -0400

    Add order create route

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   root "static_pages#root"
 
   namespace :api, defaults: {format: :json} do
+    resource :session, only: [:create, :destroy, :show]
     resources :users
+    resources :products, only: [:index, :create, :show]
     resources :carts do
       resources :cart_items
     end
-    resource :session, only: [:create, :destroy, :show]
-    resources :products, only: [:index, :create, :show]
+    resources :orders, only: [:create]
   end
 end
