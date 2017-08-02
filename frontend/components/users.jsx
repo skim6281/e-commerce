eae4544d767da39611cdda6661757f6a9836ff36
skim6281 <skim6281@gmail.com>
2017-08-02 16:05:59 -0400

    Create Users component

---
+++ frontend/components/users.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchUsers } from '../actions/user_actions';
+
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.fetchUsers();
+  }
+
+  renderUsers() {
+    console.log(this.props.users);
+    return this.props.users.map(user => {
+      return (
+        <div key={user.id}>
+          {user.username}
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return(
+      <div>
+        {this.renderUsers()}
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    users: state.users
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchUsers: () => dispatch(fetchUsers())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Users);
+
+//createProduct: (product) => dispatch(createProduct(product))
