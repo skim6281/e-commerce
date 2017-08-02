636db532ab7d8599a2f541b0095bc275db7f84f1
skim6281 <skim6281@gmail.com>
2017-08-02 02:16:28 -0400

    Edit action to delete cartitems

--- app/controllers/api/carts_controller.rb
+++ app/controllers/api/carts_controller.rb
@@ -1,3 +1,10 @@
 class Api::CartsController < ApplicationController
-
+  def show
+    @cart = Cart.includes(:cart_item).find_by(user_id: params[:user_id])
+    if @cart
+      render 'api/carts/show'
+    else
+      render json: ["Cart not found"], status: 422
+    end
+  end
 end
