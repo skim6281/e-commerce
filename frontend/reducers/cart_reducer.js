c8876450ab6cf0f80129190efe66565e86f5de09
skim6281 <skim6281@gmail.com>
2017-08-02 00:07:53 -0400

    Fix remove case

--- frontend/reducers/cart_reducer.js
+++ frontend/reducers/cart_reducer.js
@@ -1,4 +1,7 @@
-import { RECEIVE_CART_ITEMS, RECEIVE_CART_ITEM } from '../actions/cart_item_actions';
+import {
+  RECEIVE_CART_ITEMS,
+  RECEIVE_CART_ITEM,
+  REMOVE_CART_ITEM } from '../actions/cart_item_actions';
 
 const initialState = []
 
@@ -9,6 +12,19 @@ const CartReducer = (state = initialState, action) => {
       return action.cartItems
     case RECEIVE_CART_ITEM:
       return [...state, action.cartItem]
+    case REMOVE_CART_ITEM:
+      const newCartItems = state.slice();
+      let index;
+      for(let i = 0; i < newCartItems.length; i++) {
+        let item = newCartItems[i];
+        if(action.cartItem.id === item.id) {
+          index = i;
+        }
+      }
+      if(index > -1) {
+        newCartItems.splice(index, 1);
+      }
+      return newCartItems;
     default:
       return state;
   }
