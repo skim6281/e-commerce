cb19105424b7ed8b21f1959f9e1f6df18a346ffb
skim6281 <skim6281@gmail.com>
2017-08-02 02:18:49 -0400

    Edit action to destroy cartitems

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,2 +1,21 @@
 class Api::OrdersController < ApplicationController
+  def create
+    @order = Order.new(user_id: params[:user_id])
+    if @order.save
+      @order.user.cart.cart_items.each do |cart_item|
+        @order_item = OrderItem.new(
+                        order_id: @order.id,
+                        product_id: cart_item.product_id,
+                        quantity: cart_item.quantity)
+
+        if !@order_item.save
+          render json: @order_item.errors.full_messages, status: 422
+        end
+      end
+      @order.user.cart.cart_items.destroy_all
+      render 'api/orders/show'
+    else
+      render json: @order.errors.full_messages, status: 422
+    end
+  end
 end
