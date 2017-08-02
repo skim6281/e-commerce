c6349e92e02fc69290e0a07eca1bacbbdd0e7b53
skim6281 <skim6281@gmail.com>
2017-08-02 00:07:08 -0400

    Add delete functionality

--- frontend/components/cart/cart_item.jsx
+++ frontend/components/cart/cart_item.jsx
@@ -3,10 +3,17 @@ import React from 'react';
 class CartItem extends React.Component {
   constructor(props) {
     super(props);
+    this.removeCartItem = this.removeCartItem.bind(this);
+  }
+
+  removeCartItem() {
+    this.props.deleteCartItem(this.props.cartItem)
   }
 
   render() {
-    const { product } = this.props;
+    const { product, cartItem } = this.props;
+    // console.log("cartitem: ");
+    // console.log(cartItem);
     return (
       <div>
         <h3> {product.name} </h3>
@@ -14,6 +21,7 @@ class CartItem extends React.Component {
         <p> {product.model} </p>
         <p> {product.sku} </p>
         <p> {product.price} </p>
+        <button onClick={this.removeCartItem}>Remove</button>
       </div>
     )
   }
