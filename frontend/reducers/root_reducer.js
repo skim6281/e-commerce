fc6d0a4f8f4ae18ff44c061b3cd342c69f7c1507
skim6281 <skim6281@gmail.com>
2017-08-01 22:04:14 -0400

    Add cart reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import SessionReducer from './session_reducer';
 import ProductReducer from './product_reducer';
+import CartReducer from './cart_reducer';
 
 const RootReducer = combineReducers({
   session: SessionReducer,
-  products: ProductReducer
+  products: ProductReducer,
+  cart: CartReducer
 });
 
 export default RootReducer;
