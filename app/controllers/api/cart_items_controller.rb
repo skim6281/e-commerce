37e5b2c2cd2849da33f38961753f58db184dafec
skim6281 <skim6281@gmail.com>
2017-08-01 21:55:34 -0400

    Change query in index action

--- app/controllers/api/cart_items_controller.rb
+++ app/controllers/api/cart_items_controller.rb
@@ -1,5 +1,7 @@
 class Api::CartItemsController < ApplicationController
   def index
+    @cart_items = CartItem.where(cart_id: params[:cart_id])
+    render 'api/cart_items/index'
   end
 
   def create
