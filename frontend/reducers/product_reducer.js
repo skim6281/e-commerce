b44f0b774c3c44856fce79fc5ecc8b4ce64d0a5d
skim6281 <skim6281@gmail.com>
2017-08-02 12:41:44 -0400

    Add receiveProduct case

--- frontend/reducers/product_reducer.js
+++ frontend/reducers/product_reducer.js
@@ -1,15 +1,19 @@
-import { RECEIVE_PRODUCTS } from '../actions/product_actions';
+import {
+  RECEIVE_PRODUCTS,
+  RECEIVE_PRODUCT } from '../actions/product_actions';
 
 const initialState = []
 
-const SessionReducer = (state = initialState, action) => {
+const ProductReducer = (state = initialState, action) => {
   Object.freeze(state)
   switch(action.type) {
     case RECEIVE_PRODUCTS:
       return action.products;
+    case RECEIVE_PRODUCT:
+      return [action.product, ...state];
     default:
       return state;
   }
 };
 
-export default SessionReducer;
+export default ProductReducer;
