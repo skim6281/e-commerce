73d7ad435e44e674236566176b7997cc88f5c17e
skim6281 <skim6281@gmail.com>
2017-08-03 14:32:02 -0400

    Configure for dump to sql file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,7 +23,8 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
-  # config.active_record.schema_format = :sql
+  config.active_record.schema_format = :sql
+  config.active_record.dump_schema_after_migration = true
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
